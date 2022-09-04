756186ff44a289500e926bfef43fd5f06bab53ac
YXY <xiaoyang.yang@ppligo.com>
2022-09-04 12:02:10 +0800

    delete master 2

--- TQGO/Class/Base/AppDelegate.swift
+++ TQGO/Class/Base/AppDelegate.swift
@@ -11,7 +11,7 @@ import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    //   master 2
+    
     var window: UIWindow?
 
     internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
