faa2fa28042ea54c37e5bf579ae827df7136bd55
YXY <xiaoyang.yang@ppligo.com>
2022-09-04 11:31:58 +0800

    master 2

--- TQGO/Class/Base/AppDelegate.swift
+++ TQGO/Class/Base/AppDelegate.swift
@@ -11,7 +11,7 @@ import RealmSwift
 //   master 1
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    //   master 2
     var window: UIWindow?
 
     internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
